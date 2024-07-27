bc89cdc8f15a90defa9b7e6faa674721ead0ee2b
fashton28 <fashton502@gmail.com>
2024-07-27 09:12:43 -0400

    finish project

--- script.js
+++ script.js
@@ -1,39 +1,20 @@
-const numbers = document.getElementsByClassName("numbers");
-let title = document.querySelector("bdo");
-const remove = document.getElementById("remove");
-const sum = document.getElementById("add");
-for (let i = 0; i < numbers.length; i++) {
-    numbers[i].addEventListener("click", function(){
-        let summative = [];
-        title.textContent = this.textContent + title.textContent;
-        const strRev =  title.textContent.split('').reverse().join('');
-        summative.push(strRev);
-        console.log(summative);
-        console.log(strRev);
+const display = document.getElementById("display")
 
-        sum.addEventListener("click", function(){
-            const addition = Number(strRev) + 10;
-            title.textContent = addition;
-            title.textContent= title.textContent.split('').reverse().join('');
-
-
-        })
-        
-
-
-        
-    })
+function appendToDisplay(input) {
+    display.value += input
 }
 
-remove.addEventListener("click",function(){
-    title.textContent = ""
+function ClearDisplay() {
+    display.value = ""
+}
 
-})
-
-
-
-
-
+function Calculate() {
+    display.value = eval(display.value)
+}
 
 
+//Things Learned
 
+//Input element is really useful since it allows us to obtain the value with .value 
+//The eval function is really useful for this type of projects.
+//It is easier to assign the onclick event listener in the html than in JS.
